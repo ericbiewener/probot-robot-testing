b8a8d1d08f1742544d7461184ec91335bb9d6165
Eric Biewener <eric.biewener0@walmart.com>
2023-05-22 16:06:14 -0700

    more - 4:06 pm - May 22

--- test-3.js
+++
@@ -1,5 +0,0 @@
-const DELAY_MIN = 0
-
-setTimeout(() => {
-  process.exit(0)
-}, DELAY_MIN * 60 * 1000)
--- test-4.js
+++
@@ -1,6 +0,0 @@
-const DELAY_MIN = 0
-// make diff
-
-setTimeout(() => {
-  process.exit(0)
-}, DELAY_MIN * 60 * 1000)
