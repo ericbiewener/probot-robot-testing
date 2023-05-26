9f7e4ee5c708cc6a2ab46666514f63fae55c5a3e
Eric Biewener <eric.biewener0@walmart.com>
2023-05-26 15:31:46 -0700

    update with seq merge ci

--- test.js
+++ test.js
@@ -1,7 +1,17 @@
-const DELAY_MIN = 0
+const { GITHUB_HEAD_REF = "" } = process.env;
 
-console.log(process.env)
+if (GITHUB_HEAD_REF.startsWith("_seq-merge/")) {
+  const DELAY_MIN = 5;
 
-setTimeout(() => {
-  process.exit(0)
-}, DELAY_MIN * 60 * 1000)
+  setTimeout(() => {
+    process.exit(0);
+  }, DELAY_MIN * 60 * 1000);
+
+} else {
+
+  const DELAY_MIN = 0;
+
+  setTimeout(() => {
+    process.exit(0);
+  }, DELAY_MIN * 60 * 1000);
+}
