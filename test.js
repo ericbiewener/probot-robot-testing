242dc22aa33f1318ca8fb9a2d7458735873d023f
Eric Biewener <eric.biewener0@walmart.com>
2023-05-29 09:54:44 -0700

    more - 9:54 am - May 29

--- test.js
+++ test.js
@@ -1,7 +1,5 @@
 const { GITHUB_HEAD_REF = "" } = process.env;
 
-console.log(process.env)
-
 if (GITHUB_HEAD_REF.startsWith("_seq-merge/")) {
   console.log('SEQUENTIAL MERGE CHECK')
   const DELAY_MIN = 5;
@@ -11,7 +9,6 @@ if (GITHUB_HEAD_REF.startsWith("_seq-merge/")) {
   }, DELAY_MIN * 60 * 1000);
 
 } else {
-
   const DELAY_MIN = 0;
 
   setTimeout(() => {
